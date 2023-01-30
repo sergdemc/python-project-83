2e01a80d27e0ff0cd24971881d15620f49bcfed0
Sergei Demchenko <hb13718@gmail.com>
2023-01-30 09:38:28 +0300

    Fix router url_checks

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -206,7 +206,7 @@ def url_checks(id):
         response.raise_for_status()
     except requests.exceptions.RequestException:
         flash('Произошла ошибка при проверке', 'alert-danger')
-        redirect(url_for('url_info', id=id))
+        return redirect(url_for('url_info', id=id))
 
     checks = parse_page(response.text)
 
